276b79f95952e31a3995544a7680e998a27c9e98
Krishan Dharmasiri <krishantha.dharmasiri@outlook.com>
2018-07-07 09:18:04 +1000

    dashboard/investor component added and routing for them set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InvestorComponent } from './investor/investor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full',redirectTo:'dashboard'},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'investor',component:InvestorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
